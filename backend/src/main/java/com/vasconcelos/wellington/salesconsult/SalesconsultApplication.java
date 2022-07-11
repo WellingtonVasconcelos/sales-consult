ea3926d8ff23423cb38fc3d0fb5de6590adfaf65
Wellington Vasconcelos <wltvasc@hotmail.com>
2022-07-11 13:03:44 -0300

    Project created

---
+++ backend/src/main/java/com/vasconcelos/wellington/salesconsult/SalesconsultApplication.java
@@ -0,0 +1,13 @@
+package com.vasconcelos.wellington.salesconsult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesconsultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesconsultApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vasconcelos/wellington/salesconsult/SalesconsultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasconcelos.wellington.salesconsult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesconsultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
