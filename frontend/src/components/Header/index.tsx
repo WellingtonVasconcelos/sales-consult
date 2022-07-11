194d9ed0479f8fbf15eeaf64b67ce7cbc21c5946
Wellington Vasconcelos <wltvasc@hotmail.com>
2022-07-11 16:01:37 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="salesconsult-logo-container">
+                <img src={logo} alt="Sales Consult" />
+                    <h1>Sales Consult</h1>
+                    <p>
+                        Desenvolvido por
+                        <a href="https://github.com/WellingtonVasconcelos ">@WellingtonVasconcelos</a>
+                    </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
